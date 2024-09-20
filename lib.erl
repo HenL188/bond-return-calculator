0733435561a801d2fc6124c243d71fc56cbe9914
HenL188 <153272326+HenL188@users.noreply.github.com>
2024-09-20 16:35:54 -0600

    added error

--- lib.erl
+++ lib.erl
@@ -1,34 +1,47 @@
 -module(lib).
--export([total_return/0]).
+-export([total_return/0, current_value/0]).
 
 current_value() ->
-    X = io:get_line("Enter current value: "),
-    try
-        string:to_float(X),
-        true
-    catch
-        error ->
-            false
+    X = io:read("Enter current value: "),
+    case X of
+        {ok, Y} ->
+            if
+                is_float(Y) == true ->
+                    Y;
+                is_float(Y) == false ->
+                    io:format("Error not a float ~n"),
+                    current_value()
+            end
     end.
 
 total_interest() ->
-    X = io:get_line("Enter total intertest: "),
-    string:to_float(X).
+    X = io:read("Enter total intertest: "),
+    case X of
+        {ok, Y} ->
+            if
+                is_float(Y) == true ->
+                    Y;
+                is_float(Y) == false ->
+                    io:format("Error not a float ~n"),
+                    total_interest()
+            end
+    end.
 
 buy_value() ->
-    X = io:get_line("Enter price brougt for: "),
-    string:to_float(X).
+    X = io:read("Enter price brougt for: "),
+    case X of
+        {ok, Y} ->
+            if
+                is_float(Y) == true ->
+                    Y;
+                is_float(Y) == false ->
+                    io:format("Error not a float ~n"),
+                    buy_value()
+            end
+    end.
 
 total_return() ->
-    X = current_value(),
-    CV = parse(X),
-    Y = total_interest(),
-    I = parse(Y),
-    Z = buy_value(),
-    BV = parse(Z),
+    CV = current_value(),
+    I = total_interest(),
+    BV = buy_value(),
     (CV + I - BV) / BV * 100.
-
-parse(X) ->
-    TL = tuple_to_list(X),
-    [HD | _] = TL,
-    HD.
