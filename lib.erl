0d3ccf31040f3642778051f85bccc8befefd6ec4
HenL188 <153272326+HenL188@users.noreply.github.com>
2024-09-20 15:03:09 -0600

    error test

--- lib.erl
+++ lib.erl
@@ -3,20 +3,22 @@
 
 current_value() ->
     X = io:get_line("Enter current value: "),
-    CV = string:to_integer(X),
-    CV.
+    try
+        string:to_float(X),
+        true
+    catch
+        error ->
+            false
+    end.
 
 total_interest() ->
     X = io:get_line("Enter total intertest: "),
-    string:to_integer(X).
-%Y = tuple_to_list(X),
-%Y.
+    string:to_float(X).
 
 buy_value() ->
     X = io:get_line("Enter price brougt for: "),
-    string:to_integer(X).
-%Y = tuple_to_list(X),
-%Y.
+    string:to_float(X).
+
 total_return() ->
     X = current_value(),
     CV = parse(X),
